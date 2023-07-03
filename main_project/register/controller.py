882ec42214eb4ac78888c9b58c94a792164a7f1a
Pratikisback <pratikgopal.cha26@gmail.com>
2023-07-03 19:14:52 +0530

    Second push of the day

--- main_project/register/controller.py
+++ main_project/register/controller.py
@@ -9,3 +9,16 @@ def add_user(new_user):
 def find_user(email):
     result = client.UserDb.UserCollection.find_one({"email": email}, {"object_id": 0})
     return result
+
+def update_Verify(email):
+        try:
+            result = client.UserDb.UserCollection.update_one({"email": email}, {"$set": {"is_verified": True}})
+            result = True if result.acknowledged else False
+            # if result.acknowledged == True:
+            #
+            #     return True
+            # else:
+            #     return False
+            return result
+        except Exception as e:
+            return str(e)
--- main_project/register/views.py
+++ main_project/register/views.py
@@ -1,9 +1,11 @@
+from datetime import datetime, timedelta
 from email.message import EmailMessage
 from main_project.register.utils import send_emails
 from main_project import app, api, Resource
 from flask import make_response, jsonify, request, Blueprint
-from main_project.register.controller import add_user, find_user
+from main_project.register.controller import add_user, find_user, update_Verify
 import smtplib
+import jwt
 
 User_Registration_Blueprint = Blueprint("Register_User", __name__)
 
@@ -14,10 +16,10 @@ class Register(Resource):
             email = request.json.get("email")
             username = request.json.get("username")
             password = request.json.get("password")
-            is_verify = False
+            is_verified = False
             role = "Admin"
             new_user = {"email": email, "username": username, "password": password,
-                        "is_verify": is_verify, "role": role}
+                        "is_verified": is_verified, "role": role}
             if not new_user:
                 return make_response(jsonify({"Message": "Fill in the details"}))
 
@@ -25,41 +27,49 @@ class Register(Resource):
             if existing_user:
                 return make_response(jsonify({"Message": "User already exists"}))
             try:
+                token_expire_time = datetime.now() + timedelta(minutes=15)
+                token_expire_time_epoch = int(token_expire_time.timestamp())
+
+                payload = {"email": email, "exp": token_expire_time_epoch}
+                print(payload)
+                token = jwt.encode(payload, "zanc", algorithm="HS256")
+                print(token)
                 a = add_user(new_user)
                 print(a)
-                #To send email to the users registering
-                # sender = "pratikgopal.cha26@gmail.com"
-                # password = "svtitclscbvbxsai"
-                # receiver = email
-                # subject = "This is a test email"
-                # body = """
-                # This is the first attempt to send email using new method from a video
-                # """
-                #
-                # em = EmailMessage()
-                # em['From'] = sender
-                # em['To'] = receiver
-                # em['Subject'] = subject
-                # em.set_content(body)
-                #
-                # with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                #     smtp.login(sender, password)
-                #     smtp.sendmail(sender, receiver, em.as_string())
-
-                if send_emails(email):
-
+                if send_emails(email, token):
                     return make_response(jsonify({"Message": "User registered successfully",
-                                              "username": username,
-                                              "email_id": email
-                                              }))
+                                                  "username": username,
+                                                  "email_id": email
+                                                  }))
                 else:
                     return make_response(jsonify({"Message": "Email not found"}))
 
-            except:
-                return make_response(jsonify({"Message": "User was not added"}))
+            except Exception as e:
+                return make_response(jsonify({"Message": str(e)}))
 
-        except:
-            return make_response(jsonify({"Message": "Info was not filled"}))
+        except Exception as e:
+            return make_response(jsonify({"Message": str(e)}))
 
 
+class VerifyTheEmail(Resource):
+    def post(self):
+        try:
+            # token = request.headers.get("authentication_key")
+            token = request.headers.get('token')
+
+            if token:
+                token_dict = jwt.decode(token, "zanc", algorithms=["HS256"])
+                email = token_dict["email"]
+                email_db = find_user(email)
+                print(email)
+                if email == email_db["email"]:
+                    a = update_Verify(email)
+                    print(a)
+                return make_response(jsonify({"Message": "You can login now"}))
+        except Exception as e:
+            return make_response(jsonify({'Message': str(e)}))
+        return make_response(jsonify({"Message": "Updated"}))
+
+
+api.add_resource(VerifyTheEmail, '/verify')
 api.add_resource(Register, '/register')
